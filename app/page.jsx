db3028973ea6f113cd2aa4d4fc0046eef80cef56
Your Name <vineetvijay17@gmail.com>
2025-12-19 11:48:34 +0530

    feat: Add how it works section and improve layout for features and stats

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/hero";
 import { Card, CardContent } from "@/components/ui/card";
-import statsData, { featuresData } from "@/data/landing";
+import statsData, { featuresData, howItWorksData } from "@/data/landing";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
 
       <section className="py-20 bg-blue-50">
         <div className="container mx-auto px-4">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+          <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {statsData.map((item, index) => (
               <div key={index} className="text-center">
                 <div className="text-4xl font-bold text-blue-600 mb-2">
@@ -25,19 +25,36 @@ export default function Home() {
       </section>
 
       <section className="py-20">
-        <div>
-            <h2>
+        <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center mb-12">
               Everything you need to manage your finances in one place.
             </h2>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8">
               {featuresData.map((feature, index) => (
-                <Card>
-                  <CardContent>
+                <Card key={index} className="p-6">
+                  <CardContent className="space-y-4 pt-4">
                     {feature.icon}
-                    <h3>{feature.title}</h3>
-                    <p>{feature.description}</p>
+                    <h3 className="text-xl font-semibold">{feature.title}</h3>
+                    <p className="text-gray-500">{feature.description}</p>
                   </CardContent>
                 </Card>
+              ))}
+            </div>
+        </div>
+      </section>
+
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold text-center mb-16">
+              How it works
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+              {howItWorksData.map((step, index) => (
+                <div key={index} className="text-center">
+                  <div className="w-16 h-16 bg-blue-100 rounded-full flex items-center justify-center mx-auto mb-6" >{step.icon}</div>
+                  <div className="text-lg font-semibold mb-4">{step.title}</div>
+                  <div className="text-gray-500">{step.description}</div>
+                </div>
               ))}
             </div>
         </div>
